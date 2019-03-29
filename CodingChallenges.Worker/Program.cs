6494b6b8d4e07d865f1cc2046cf38fa5e6c57dc3
Laurentiu Stamate <lastamat@microsoft.com>
2019-03-29 08:40:13 +0100

    Add console debug project

---
+++ CodingChallenges.Worker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingChallenges.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO Do your magical debugging here!
+        }
+    }
+}
