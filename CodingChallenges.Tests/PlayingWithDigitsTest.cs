a5faa60fc09b28eb4c5133025b987e3f123c0dd1
ZeChArtiahSaher <43679179+ZeChArtiahSaher@users.noreply.github.com>
2019-04-13 17:13:04 +0200

    Fixing unfinished asserts

--- CodingChallenges.Tests/PlayingWithDigitsTest.cs
+++ CodingChallenges.Tests/PlayingWithDigitsTest.cs
@@ -11,12 +11,14 @@ namespace CodingChallenges.Tests
             var actual = PlayingWithDigits.ComputeNumber(89, 1);
             Assert.Equal(1, actual);
 
-            actual = PlayingWithDigits.ComputeNumber(89, 1);
-            Assert.Equal(1, actual);
+            actual = PlayingWithDigits.ComputeNumber(92, 1);
+            Assert.Equal(-1, actual);
 
-            actual = PlayingWithDigits.ComputeNumber(89, 1);
-            Assert.Equal(1, actual);
+            actual = PlayingWithDigits.ComputeNumber(695, 2);
+            Assert.Equal(2, actual);
 
+            actual = PlayingWithDigits.ComputeNumber(46288, 3);
+            Assert.Equal(51, actual);
         }
     }
 }
